21f0c2df78087def09ee86261130484cee486759
brfinanceirooficial-hash <brfinanceirooficial@gmail.com>
2026-06-29 22:21:21 -0300

    Ajuste de textos e adicao da pagina sobre

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
         produtos: resolve(__dirname, 'produtos.html'),
         admin:   resolve(__dirname, 'admin.html'),
         code:    resolve(__dirname, 'code.html'),
+        sobre:   resolve(__dirname, 'sobre.html'),
       }
     }
   }
